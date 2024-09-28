d959bcffbeb140c590ea30f889abf1fb2233d84a
lucasreis <lucaschristian10@gmail.com>
2024-09-28 13:08:06 -0300

    feat: criar transicao em componente Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,30 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handlerScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handlerScroll);
+
+    return () => window.removeEventListener("scroll", handlerScroll);
+  }, []);
+
   return (
-    <header className="fixed top-0 z-50 flex w-full  items-center justify-between transition-all p-4 lg:px-10 py-6">
+    <header
+      className={`
+      ${isScrolled && "bg-black"}
+      fixed top-0 z-50 flex w-full items-center justify-between transition-all p-4 lg:px-10 py-6`}
+    >
       <div className="flex items-center space-x-2 md:space-x-8">
         <Image alt="LOGO" src="/logo.svg" width={120} height={120}></Image>
         <ul className="hidden md:flex md:space-x-8">
