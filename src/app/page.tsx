d6dd9c85256248f83b2d5f20eeb2d3363fb1bce4
lucasreis <lucaschristian10@gmail.com>
2024-09-28 11:52:41 -0300

    feat: adicionar banner em pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,31 @@ export default function Home() {
     <div className="relative h-full  lg:h-[140vh]">
       <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
         <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-          <div className="absolute flex top-0 left-0 -z-10 h-[95vh] flex-col w-screen bg-black"></div>
-
+          <div className="absolute flex top-0 left-0 -z-10 h-[95vh] flex-col w-screen bg-black">
+            <Image
+              src="/banner.png"
+              alt="Maid"
+              fill={true}
+              className="h-[65v] object-cover object-top lg:h-[95vh]"
+            />
+          </div>
           <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
             The Witcher
           </h1>
+
+          <p className="text-shadow-md max-w-xs text-xs md:text-w-lg md:text-lg lg:max-w-2xl">
+            Geralt of Rivia, a solitary monster hunter, struggles to find his
+            place in a world where people often prove more wicked then beasts.
+          </p>
+        </div>
+
+        <div className="flex space-x-3">
+          <button className="md:text-xl; flex cursor-pointer items-center rounded gap-x-2 bg-white text-black px-5 py-1.5 text-sm font-semibold transition hover:opacity-75 md:px-8 md:py-2.5">
+            Play
+          </button>
+          <button className="md:text-xl; flex cursor-pointer items-center rounded gap-x-2 bg-gray-500 text-black px-5 py-1.5 text-sm font-semibold transition hover:opacity-75 md:px-8 md:py-2.5">
+            More Info
+          </button>
         </div>
       </main>
     </div>
