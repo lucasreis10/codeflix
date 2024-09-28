66a8c71b015e9943674f2cae89e8bcd3719a03da
lucasreis <lucaschristian10@gmail.com>
2024-09-28 12:53:24 -0300

    feat: adicionar profile em componente header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 z-50 flex w-full bg-black items-center justify-between p-4 lg:px-10 py-6">
-      <div className="flex items-center space-x-2 md:space-x-4 ">
+    <header className="fixed top-0 z-50 flex w-full  items-center justify-between transition-all p-4 lg:px-10 py-6">
+      <div className="flex items-center space-x-2 md:space-x-8">
         <Image alt="LOGO" src="/logo.svg" width={120} height={120}></Image>
         <ul className="hidden md:flex md:space-x-8">
           <li>Home</li>
@@ -11,6 +11,16 @@ export const Header = () => {
           <li> Movies</li>
           <li>Latest</li>
         </ul>
+      </div>
+      <div className="flex space-x-4">
+        <p className="hidden cursor-not-allowed lg:inline">Kids</p>
+        <Image
+          className="cursor-pointer rounded"
+          src="/profile.png"
+          alt="PROFILE"
+          width={30}
+          height={30}
+        />
       </div>
     </header>
   );
