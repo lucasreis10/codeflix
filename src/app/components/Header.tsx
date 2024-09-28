924631121f015253f9b1f950ebaf4d0f601667c3
lucasreis <lucaschristian10@gmail.com>
2024-09-28 13:16:18 -0300

    refactor: modularizar componente Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,44 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { Component, useEffect, useState } from "react";
+
+const Logo = () => {
+  return (
+    <Image
+      className="cursor-pointer"
+      alt="LOGO"
+      src="/logo.svg"
+      width={120}
+      height={120}
+    ></Image>
+  );
+};
+
+const NavLinks = () => {
+  return (
+    <ul className="hidden md:flex md:space-x-8">
+      <li>Home</li>
+      <li>TV Shows</li>
+      <li>Movies</li>
+      <li>Latest</li>
+    </ul>
+  );
+};
+
+const UserProfile = () => {
+  return (
+    <div className="flex space-x-4">
+      <p className="hidden cursor-not-allowed lg:inline">Kids</p>
+      <Image
+        className="cursor-pointer rounded"
+        src="/profile.png"
+        alt="PROFILE"
+        width={40}
+        height={40}
+      />
+    </div>
+  );
+};
 
 export const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -26,24 +64,10 @@ export const Header = () => {
       fixed top-0 z-50 flex w-full items-center justify-between transition-all p-4 lg:px-10 py-6`}
     >
       <div className="flex items-center space-x-2 md:space-x-8">
-        <Image alt="LOGO" src="/logo.svg" width={120} height={120}></Image>
-        <ul className="hidden md:flex md:space-x-8">
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li> Movies</li>
-          <li>Latest</li>
-        </ul>
+        <Logo />
+        <NavLinks />
       </div>
-      <div className="flex space-x-4">
-        <p className="hidden cursor-not-allowed lg:inline">Kids</p>
-        <Image
-          className="cursor-pointer rounded"
-          src="/profile.png"
-          alt="PROFILE"
-          width={30}
-          height={30}
-        />
-      </div>
+      <UserProfile />
     </header>
   );
 };
