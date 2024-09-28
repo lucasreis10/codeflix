71fa88cbad3bc9fd9b0c2e7a83402c20e40c96cb
lucasreis <lucaschristian10@gmail.com>
2024-09-28 12:40:54 -0300

    ajustar itens do componente header para apresentação horizontal

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,13 +3,15 @@ import Image from "next/image";
 export const Header = () => {
   return (
     <header className="fixed top-0 z-50 flex w-full bg-black items-center justify-between p-4 lg:px-10 py-6">
-      <Image alt="LOGO" src="/logo.svg" width={120} height={120}></Image>
-      <ul>
-        <li>Home</li>
-        <li>TV Shows</li>
-        <li> Movies</li>
-        <li>Latest</li>
-      </ul>
+      <div className="flex items-center space-x-2 md:space-x-4 ">
+        <Image alt="LOGO" src="/logo.svg" width={120} height={120}></Image>
+        <ul className="hidden md:flex md:space-x-8">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li> Movies</li>
+          <li>Latest</li>
+        </ul>
+      </div>
     </header>
   );
 };
